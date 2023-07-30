316412ef81540ed118e586b8d5d96f97bb365457
Don Apollo Cosio <dacosio@gmail.com>
2023-07-30 15:41:46 -0700

    changes

--- patterns/header-cover.php
+++ patterns/header-cover.php
@@ -7,20 +7,24 @@
  */
 ?>
 <!-- wp:group {"align":"full","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull"><!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Hero.png","id":8,"dimRatio":50,"overlayColor":"black","minHeight":50,"contentPosition":"center center","align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}}} -->
-<div class="wp-block-cover alignfull" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);min-height:50px"><span aria-hidden="true" class="wp-block-cover__background has-black-background-color has-background-dim"></span><img class="wp-block-cover__image-background wp-image-8" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Hero.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0","left":"40px","right":"40px"}}},"textColor":"base","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} -->
+<div class="wp-block-group alignfull"><!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Hero.png","id":8,"dimRatio":50,"overlayColor":"black","minHeight":362,"minHeightUnit":"px","contentPosition":"center center","align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}}} -->
+<div class="wp-block-cover alignfull" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);min-height:362px"><span aria-hidden="true" class="wp-block-cover__background has-black-background-color has-background-dim"></span><img class="wp-block-cover__image-background wp-image-8" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Hero.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0","left":"40px","right":"40px"}}},"textColor":"base","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} -->
 <div class="wp-block-group alignwide has-base-color has-text-color has-link-color" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:40px;padding-bottom:0;padding-left:40px"><!-- wp:site-title {"style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary","fontSize":"large"} /-->
 
 <!-- wp:navigation {"ref":91,"textColor":"white","icon":"menu","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"}} /--></div>
 <!-- /wp:group -->
 
-<!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontSize":"88px","fontStyle":"normal","fontWeight":"700"},"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"textColor":"primary"} -->
-<h1 class="wp-block-heading has-text-align-center has-primary-color has-text-color" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;font-size:88px;font-style:normal;font-weight:700">UNLOCK<br>the VAULT</h1>
-<!-- /wp:heading -->
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
 
-<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"fontSize":"medium"} -->
-<p class="has-text-align-center has-medium-font-size" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;font-style:normal;font-weight:700">Langara Film Arts Festival 2023</p>
-<!-- /wp:paragraph -->
+<!-- wp:kadence/advancedheading {"uniqueID":"_b048a4-71","align":"center","color":"#FF5FD2","markBorder":"","markBorderStyles":[{"top":[null,"",""],"right":[null,"",""],"bottom":[null,"",""],"left":[null,"",""],"unit":"px"}],"tabletMarkBorderStyles":[{"top":[null,"",""],"right":[null,"",""],"bottom":[null,"",""],"left":[null,"",""],"unit":"px"}],"mobileMarkBorderStyles":[{"top":[null,"",""],"right":[null,"",""],"bottom":[null,"",""],"left":[null,"",""],"unit":"px"}],"colorClass":"primary","fontSize":[120,100,68]} -->
+<h2 class="kt-adv-heading_b048a4-71 wp-block-kadence-advancedheading has-primary-color has-text-color" data-kb-block="kb-adv-heading_b048a4-71"><strong>UNLOCK<br>the VAULT</strong></h2>
+<!-- /wp:kadence/advancedheading -->
+
+<!-- wp:kadence/advancedheading {"uniqueID":"_8fd606-58","align":"center","color":"#FFFFFF","markBorder":"","markBorderStyles":[{"top":[null,"",""],"right":[null,"",""],"bottom":[null,"",""],"left":[null,"",""],"unit":"px"}],"tabletMarkBorderStyles":[{"top":[null,"",""],"right":[null,"",""],"bottom":[null,"",""],"left":[null,"",""],"unit":"px"}],"mobileMarkBorderStyles":[{"top":[null,"",""],"right":[null,"",""],"bottom":[null,"",""],"left":[null,"",""],"unit":"px"}],"colorClass":"contrast","fontSize":[40,26,""]} -->
+<h2 class="kt-adv-heading_8fd606-58 wp-block-kadence-advancedheading has-contrast-color has-text-color" data-kb-block="kb-adv-heading_8fd606-58"><strong>Langara Film Arts Festival 2023</strong></h2>
+<!-- /wp:kadence/advancedheading -->
 
 <!-- wp:spacer -->
 <div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
